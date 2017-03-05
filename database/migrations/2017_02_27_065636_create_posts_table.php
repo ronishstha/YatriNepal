037eb4d9fb8699a3396e4a72c3e384a9324fbf00
Ronish Shrestha <stharonish@gmail.com>
2017-03-05 16:32:16 +0545

    setting workspace for cms

---
+++ database/migrations/2017_02_27_065636_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('title');
+            $table->string('page');
+            $table->text('content');
+            $table->integer('category_id');
+            $table->text('slug');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
